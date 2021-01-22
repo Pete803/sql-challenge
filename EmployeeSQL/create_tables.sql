85e7f6c03ddb1a3b82d6bad49a4bec609653195f
Lance Ito <70550863+Pete803@users.noreply.github.com>
2021-01-22 13:27:07 -0500

    update 2

---
+++ EmployeeSQL/create_tables.sql
@@ -0,0 +1,42 @@
+CREATE TABLE departments (
+	dept_no VARCHAR(30),
+	dept_name VARCHAR(30),
+	PRIMARY KEY (dept_no)
+);
+
+
+CREATE TABLE dept_emp (
+	emp_no INT,
+	dept_no VARCHAR(30),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(30),
+	emp_no INT,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+
+CREATE TABLE titles(
+	title_id VARCHAR(30),
+	title VARCHAR(30)
+);
+
+
+CREATE TABLE employees(
+	emp_no SERIAL PRIMARY KEY,
+	emp_title_id VARCHAR(30),
+	birth_date VARCHAR(30),
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+	sex VARCHAR(30),
+	hire_date VARCHAR(30)
+);
+
+
+CREATE TABLE salaries(
+	emp_no SERIAL PRIMARY KEY,
+	salary INTEGER
+);
