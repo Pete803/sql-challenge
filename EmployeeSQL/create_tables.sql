d8f3f0ad03427f2ad69fb19e9640548236398e7a
Lance Ito <70550863+Pete803@users.noreply.github.com>
2021-01-22 15:12:22 -0500

    update

--- EmployeeSQL/create_tables.sql
+++ EmployeeSQL/create_tables.sql
@@ -1,8 +1,34 @@
-SELECT employees.emp_no,
-	   employees.last_name,
-	   employees.first_name,
-	   employees.sex,
-	   salaries.salary
-FROM employees
-INNER JOIN salaries
-	ON salaries.emp_no = employees.emp_no;
+CREATE TABLE departments(
+	dept_no VARCHAR(30),
+	dept_name VARCHAR(30)	
+);
+
+CREATE TABLE dept_emp (
+	emp_no Integer,
+	dept_no VARCHAR(30)
+);
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(30),
+	emp_no SERIAL PRIMARY KEY
+);
+
+CREATE TABLE employees(
+	emp_no SERIAL PRIMARY KEY,
+	emp_title_id VARCHAR(30),
+	birth_date VARCHAR(30),
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+	sex VARCHAR(30),
+	hire_date VARCHAR(30)
+);
+
+CREATE TABLE salaries(
+	emp_no SERIAL PRIMARY KEY,
+	salary INTEGER
+);
+
+CREATE TABLE titles(
+	title_id VARCHAR(30),
+	title VARCHAR(30)
+);
